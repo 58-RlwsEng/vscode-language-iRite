cd5fd77cd0406e455b15c6a03dbb496cbe4d9930
Kyle Eggleston <KEggleston@RiceLake.com>
2018-12-21 16:58:19 -0600

    Fixed issue with iRite console not showing proper information

--- Release.bat
+++ Release.bat
@@ -1,37 +1,4 @@
 call deploy.bat
-echo. 
-Title RELEASE Public
-echo. 
-SET BROWSER=chrome.exe
-SET WAIT_TIME=2
-start "" chrome --app=http://github.com/58-RlwsEng/vscode-language-iRite/releases
-echo. 
-for /F "delims=" %%A in (Comment.txt) do set %%A
-set /p Tag= Enter Tag: 
-echo ^#^#^#^# %Tag%>>"readme.md"
-echo     * %Comment%>>"readme.md"
-set /p Prerelease= Prerelease?(true or false): 
-git add --all
-git commit -am "%Tag% %Comment%"
-git remote remove origin
-git remote add origin http://github.com/58-RlwsEng/vscode-language-iRite.git
-git push -u origin master
-echo. 
-git commit -am "ReadMe"
-git push -u origin master
-curl -u 58-RlwsEng:Rlws2349171 --data "{\"tag_name\": \"V.%Tag%\",\"target_commitish\": \"master\",\"name\": \"%Tag%\",\"body\": \"Release of version %Tag%. %comment% \",\"draft\": false,\"prerelease\": %Prerelease%}"  http://github.com/api/v3/repos/58-RlwsEng/vscode-language-iRite/releases
-echo.
-git push -u origin master -f
-del comment.txt
-echo. 
-echo Release complete.  Press Any Key.
-taskkill /fi "WindowTitle eq Releases*"
-pause >nul
-start "" chrome --app=http://github.com/58-RlwsEng/vscode-language-iRite/releases
-echo Ensure Correct Version Shown
-pause >nul
-taskkill /fi "WindowTitle eq Releases*"
-
 Title RELEASE Enterprise
 echo. 
 SET BROWSER=chrome.exe
@@ -53,7 +20,7 @@ git commit -am "ReadMe"
 git push -u origin master
 curl -u kylegg:eczqiofw --data "{\"tag_name\": \"V.%Tag%\",\"target_commitish\": \"master\",\"name\": \"%Tag%\",\"body\": \"Release of version %Tag%. %comment% \",\"draft\": false,\"prerelease\": %Prerelease%}" http://github.corp.rlws.com/api/v3/repos/RiceLake-Engineering/vscode_irite/releases
 echo.
-git push -u origin master -f
+git push -u origin master
 del comment.txt
 echo. 
 echo Release complete.  Press Any Key.
